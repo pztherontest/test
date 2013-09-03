e6adc30881c0769f077e74eef2aa88a74be8113b
Tyler Allen <drpennybags@gmail.com>
2013-09-03 13:38:15 -0500

    Revert 37f722b..e491f2a
    This rolls back to commit 37f722bdd7ddfddc5a0b183ccd9f2f1070ec5f0d.

---
+++ AjaxServerControl2/AjaxServerControl2/ClientControl1.js
@@ -0,0 +1,23 @@
+﻿/// <reference name="MicrosoftAjax.js"/>
+
+
+Type.registerNamespace("AjaxServerControl2");
+
+AjaxServerControl2.ClientControl1 = function(element) {
+    AjaxServerControl2.ClientControl1.initializeBase(this, [element]);
+}
+
+AjaxServerControl2.ClientControl1.prototype = {
+    initialize: function() {
+        AjaxServerControl2.ClientControl1.callBaseMethod(this, 'initialize');
+        
+        // Add custom initialization here
+    },
+    dispose: function() {        
+        //Add custom dispose actions here
+        AjaxServerControl2.ClientControl1.callBaseMethod(this, 'dispose');
+    }
+}
+AjaxServerControl2.ClientControl1.registerClass('AjaxServerControl2.ClientControl1', Sys.UI.Control);
+
+if (typeof(Sys) !== 'undefined') Sys.Application.notifyScriptLoaded();

254b06cf70565c0171ed9692e3699f938a9f8aa3
Paul z Theron <pztheron@gmail.com>
2013-09-03 12:22:05 -0500

    First commit to the repository. I added a few new projects

---
+++ AjaxServerControlProject/AjaxServerControl1/AjaxServerControl1/Properties/AssemblyInfo.cs
@@ -0,0 +1,39 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Web.UI;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("AjaxServerControl1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("AjaxServerControl1")]
+[assembly: AssemblyCopyright("Copyright ©  2013")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("d4955232-02eb-4207-bf78-ac7ca53aed89")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: WebResource("AjaxServerControl1.ClientControl1.js", "text/javascript")]
+[assembly: ScriptResource("AjaxServerControl1.ClientControl1.js",
+   "AjaxServerControl1.ClientControl1", "AjaxServerControl1.Resource")]
